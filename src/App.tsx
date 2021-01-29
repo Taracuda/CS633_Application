ef25e546d552fa77e97753950a4482909e589d6e
Tara <tlynnliv@bu.edu>
2021-01-28 18:20:27 -0600

    Cleanup of files. Placeholder logo added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CollectRImage from './images/CollectR.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={CollectRImage} className="CollectR-logo" alt="CollectR-logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to our term project!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out React
         </a>
       </header>
     </div>
