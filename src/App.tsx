b6da7cb5df8c89c9d9be03b74daaf0b9b4c23af6
Tara <tlynnliv@bu.edu>
2021-02-08 20:25:09 -0600

    Created PageLayoutComponent, separated landing page and other pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { HeaderComponent } from './components/HeaderComponent';
 import { LoginComponent } from './components/LoginComponent';
-import CollectRImage from './images/CollectRImage.png'
+import { PageLayoutComponent } from './components/PageLayoutComponent';
 import IAuthState from './domain/IAuthState';
 
 function App() {
@@ -22,46 +22,16 @@ function App() {
   };
 
   return (
-    <div className='wrapper'>
-      <div className='main-container container'>
-        <div className='child item'>
-          <div className='left-side'>
-            <div className='left-box'>
-              <h1 className='box-text'>Box 1</h1>
-            </div>
-            <div className='left-box'>
-              <h1 className='box-text'>Box 2</h1>
-            </div>
-            <div className='left-box'>
-              <h1 className='box-text'>Box 3</h1>
-            </div>
-          </div>
-          <div className="App">
-              <LoginComponent authState={authState}
-                onAuthStateChanged={authStateChange}
-              />
-          </div>
-        </div>
-        <div className='child item-center'>
-          <div className='right-side'>
-            <div className='right-row'>
-              <div className='right-item'><h1 className='box-text'>Box 1</h1></div>
-              <div className='right-item'><h1 className='box-text'>Box 2</h1></div>
-              <div className='right-item'><h1 className='box-text'>Box 3</h1></div>
-            </div>
-            <div className='right-row'>
-              <div className='right-item'><h1 className='box-text'>Box 4</h1></div>
-              <div className='right-item'><h1 className='box-text'>Box 5</h1></div>
-              <div className='right-item'><h1 className='box-text'>Box 6</h1></div>
-            </div>
-
-
-
-          </div>
-        </div>
-      </div>
-    </div>
-
+    //For now, this PageLayoutComponent tag is allowing you to see the page you're working on (PageLayoutComponent.tsx) 
+    //instead of the initial landing page. Let me know before you make a PR and we'll revert this back.
+    //The HeaderComponent tag contains the CollectR image (HeaderComponent.tsx)
+    <PageLayoutComponent />
+          // <div className="App">
+          //     <HeaderComponent />
+          //     <LoginComponent authState={authState}
+          //       onAuthStateChanged={authStateChange}
+          //     />
+          // </div>
   );
 }
 
---
+++ src/components/PageLayoutComponent.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const PageLayoutComponent: React.FC = () => {
+
+    return (
+        <div className='wrapper'>
+            <div className='main-container container'>
+                <div className='child item'>
+                    <div className='left-side'>
+                        <div className='left-box'>
+                            <h1 className='box-text'>Box 1</h1>
+                        </div>
+                        <div className='left-box'>
+                            <h1 className='box-text'>Box 2</h1>
+                        </div>
+                        <div className='left-box'>
+                            <h1 className='box-text'>Box 3</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='child item-center'>
+                    <div className='right-side'>
+                        <div className='right-row'>
+                            <div className='right-item'><h1 className='box-text'>Box 1</h1></div>
+                            <div className='right-item'><h1 className='box-text'>Box 2</h1></div>
+                            <div className='right-item'><h1 className='box-text'>Box 3</h1></div>
+                        </div>
+                        <div className='right-row'>
+                            <div className='right-item'><h1 className='box-text'>Box 4</h1></div>
+                            <div className='right-item'><h1 className='box-text'>Box 5</h1></div>
+                            <div className='right-item'><h1 className='box-text'>Box 6</h1></div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );    
+}   
