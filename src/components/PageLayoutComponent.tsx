82ff3d10703beb4dc3296ca0224df88ae74ff465
Tara <tlynnliv@bu.edu>
2021-02-13 12:00:22 -0600

    Satisfy checks again

--- src/components/PageLayoutComponent.tsx
+++ src/components/PageLayoutComponent.tsx
@@ -1,4 +1,4 @@
-import React, { cloneElement, useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CollectRImage from "../images/CollectR.jpg";
 import { observer } from "mobx-react-lite";
 import "./PageLayoutComponent.css";
