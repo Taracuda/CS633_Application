d16a56e725868166cea37bfd96afcbedc751f2e0
Tara <tlynnliv@bu.edu>
2021-02-21 13:00:19 -0600

    Navbar addition of favorites link

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -13,6 +13,7 @@ export const NavbarComponent: React.FC = () => {
       <Nav.Link href="/home">  Home  </Nav.Link>
       <Nav.Link href="/dashboard">  Dashboard  </Nav.Link>
       <Nav.Link href="/browse">  Browse  </Nav.Link>
+      <Nav.Link href="/favorites">  Favorites  </Nav.Link>
       <Nav.Link href="https://github.com/Taracuda/CS633_Application/issues">  Questions/Comments?  </Nav.Link>
       <button className="sign-out-button"onClick={() => {Auth.signOut()}}><Link to={"/home"}>Sign Out</Link></button>
     </Nav>
