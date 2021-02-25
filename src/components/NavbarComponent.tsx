f10413e473007ca457c8a29a7cb0b46614c9ab1b
Tara <tlynnliv@bu.edu>
2021-02-25 11:02:54 -0600

    minor css change

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -7,13 +7,13 @@ export const NavbarComponent: React.FC = () => {
 
     return (
         <>
-    <div className="navbar">
-        <Link to="/home" className="navbar">  Home  </Link><br/><br/>
-        <Link to="/dashboard" className="navbar">  Dashboard  </Link><br/><br/>
-        <Link to="/browse" className="navbar">  Browse  </Link><br/><br/>
-        <Link to="/favorites" className="navbar">  Favorites  </Link><br/><br/>
-        <a href="https://github.com/Taracuda/CS633_Application/issues" target="_blank" className="navbar">  Feedback  </a><br/><br/>
-        <Link to="/home" onClick={() => {Auth.signOut()}} className="navbar">  Sign Out  </Link>
+    <div className="navbar-links">
+        <Link to="/home" className="navbar-links">  Home  </Link><br/><br/>
+        <Link to="/dashboard" className="navbar-links">  Dashboard  </Link><br/><br/>
+        <Link to="/browse" className="navbar-links">  Browse  </Link><br/><br/>
+        <Link to="/favorites" className="navbar-links">  Favorites  </Link><br/><br/>
+        <a href="https://github.com/Taracuda/CS633_Application/issues" target="_blank" className="navbar-links">  Feedback  </a><br/><br/>
+        <Link to="/home" onClick={() => {Auth.signOut()}} className="navbar-links">  Sign Out  </Link>
     </div>
         </>
     );}
